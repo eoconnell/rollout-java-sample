8837dff1beaf8010782b175e3430c6b873375108
Evan O'Connell <oconn.e@gmail.com>
2016-06-12 11:24:46 -0400

    Add simple example using rollout-java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,42 @@
 
 import static spark.Spark.*;
 
+import com.evanoconnell.rollout.*;
+import com.evanoconnell.rollout.storage.*;
+
 public class App {
+
+  static class User implements IRolloutUser {
+    private long id;
+    public User(long id) {
+      this.id = id;
+    }
+    public long getId() {
+      return id;
+    }
+  }
 
   public static void main(String[] args) {
 
+    IRolloutStorage storage = new MapStorage();
+    final Rollout<User> rollout = new Rollout<>(storage);
+
     port(Integer.valueOf(System.getenv("PORT")));
 
+    get("/feature/activate", (request, response) -> {
+      rollout.activatePercentage("feature", 100);
+      return "activated feature for all ";
+    });
+
+    get("/feature/deactivate", (request, response) -> {
+      rollout.activatePercentage("feature", 0);
+      return "deactivated feature for all ";
+    });
+
     get("/feature/:user", (request, response) -> {
-      return "User: " + request.params(":user");
+      long id = Long.parseLong(request.params(":user"), 10);
+      boolean isActive = rollout.isActive("feature", new User(id));
+      return "User: " + request.params(":user") + "\nActive? " + isActive;
     });
   }
 
